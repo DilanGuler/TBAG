1028fdc464ed7c17565da34021307d3df362c1ba
DilanGuler <dilanguleruk@gmail.com>
2024-06-13 12:47:17 +0100

    Update main.py
    update

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ kitchen = Room("kitchen")
 ballroom = Room("ballroom")
 dining_hall = Room("dining hall")
 
-kitchen.set_description("A dank and dirty room buzzing with flies")
-ballroom.set_description("A vast room with a shiny wooden floor")
+kitchen.set_description("A dark and dirty room buzzing with flies")
+ballroom.set_description("A big room with a shiny wooden floor")
 dining_hall.set_description("A large room with ornate golden decorations")
